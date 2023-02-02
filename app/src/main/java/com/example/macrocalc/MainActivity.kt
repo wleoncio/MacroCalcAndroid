e153f1317ed1aeacbaa7f70693f652c4d3229110
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-02-02 12:05:29 +0100

    Calculating kcal per gram of fat

--- app/src/main/java/com/example/macrocalc/MainActivity.kt
+++ app/src/main/java/com/example/macrocalc/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.macrocalc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Assign references to relevant UI objects
+        val sentSubmission: Button = findViewById(R.id.submitButton)
+        val fatW: EditText = findViewById(R.id.fatWeight)
+
+        // Start calculations when button is pressed
+        sentSubmission.setOnClickListener {
+            calcMacros(fatW, R.id.textView)
+        }
+    }
+
+    /* Calculate macros */
+    private fun calcMacros(fatW: EditText, targetView: Int) {
+        // Create a food item
+        val food = Food(fatW)
+
+        // Return energy information
+        val kcal: TextView = findViewById(targetView)
+        kcal.text = "${food.calcCal(fatW)} kcal from fat"
+    }
+}
+
+/* Food item with macronutrient information */
+class Food(private val fatGrams: EditText) {
+    fun calcCal(grams: EditText): Double {
+        val numGrams = grams.text.toString().toDouble()
+        return 9 * numGrams
     }
 }
